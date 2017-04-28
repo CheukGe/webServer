ea3489d81c57cbeaa4691c74984bf7426bd86935
ZYchao98 <15766590884@163.com>
2017-04-28 22:58:42 +0800

    Revert "website"
    This reverts commit cb8e191e529d8365b432a406d7208dfb8edbcfc5.

--- php/deluser.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("content-type:text/html;charset=utf8");
-setcookie("username","",time(),"/");
-setcookie("password","",time(),"/");
-echo "<script>alert('返回登录');location.href='../index.php'</script>";
-?>
